1dec57aaf95f27bdd7fc7f13d9f90a4757c6867a
syunta <kitagawa@timedia.co.jp>
2019-05-15 15:12:53 +0900

    Add test.sh

---
+++ test.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+try() {
+    expected="$1"
+    input="$2"
+
+    ./9cc "$input" > tmp.s
+    gcc -o tmp tmp.s
+    ./tmp
+    actual="$?"
+
+    if [[ "$actual" = "$expected" ]]; then
+        echo "$input => $actual"
+    else
+        echo "$expected expected, but got $actual"
+        exit 1
+    fi
+}
+
+gcc -o 9cc 9cc.c
+
+try 0 0
+try 42 42
+
+echo OK
