960f6a76bf56836ca6d1eb03011cc7a593fe6463
syunta <kitagawa@timedia.co.jp>
2019-09-03 07:34:03 +0900

    Add tokenize.c

---
+++ 9cc.h
@@ -0,0 +1,30 @@
+#include <ctype.h>
+#include <stdarg.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef enum {
+    TK_RESERVED,
+    TK_NUM,
+    TK_EOF,
+} TokenKind;
+
+typedef struct Token Token;
+
+struct Token {
+    TokenKind kind;
+    Token *next;
+    int val;
+    char *str;
+    int len;
+};
+
+extern Token *token;
+extern char *user_input;
+
+bool consume(char *op);
+void expect(char *op);
+int expect_number();
+Token *tokenize();
